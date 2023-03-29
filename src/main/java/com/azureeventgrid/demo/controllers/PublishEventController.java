c895d1183d603e0ac4daa5cc495a6fd5a1e2d2c2
0038X2744 <0038X2744@IBM-PF3G7TGN.in.ibm.com>
2023-03-29 13:49:22 +0530

    redis changes

--- src/main/java/com/azureeventgrid/demo/controllers/PublishEventController.java
+++ src/main/java/com/azureeventgrid/demo/controllers/PublishEventController.java
@@ -1,8 +1,19 @@
 package com.azureeventgrid.demo.controllers;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
+import com.azure.messaging.eventgrid.EventGridPublisherClient;
+import com.azure.messaging.eventgrid.EventGridPublisherClientBuilder;
 
 @RestController
 public class PublishEventController {
@@ -15,7 +26,32 @@ public class PublishEventController {
 
     @GetMapping("/publish-event")
     String publishEvent() {
-        return "This is an event.";
+    	
+    	// For custom event
+    	EventGridPublisherClient<BinaryData> customEventClient = new EventGridPublisherClientBuilder()
+    	    .endpoint("https://pricing-data-topic.westeurope-1.eventgrid.azure.net/api/events")
+    	    .credential(new AzureKeyCredential("k7ytifcZVmlD5Pwnz1olalsCOU6dGpESxGOBkkzX3xA="))
+    	    .buildCustomEventPublisherClient();
+    	
+    	// Make sure that the event grid topic or domain you're sending to is able to accept the custom event schema.
+    	List<BinaryData> events = new ArrayList<>();
+    	events.add(BinaryData.fromObject(new HashMap<String, String>() {
+    	    /**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			
+			{
+    	        put("id", UUID.randomUUID().toString());
+    	        put("eventTime", OffsetDateTime.now().toString());
+    	        put("subject", "Pricing");
+    	        put("eventType", "pricingManagement.Data");
+    	        put("data", "[{'itemName':'<item-price>'}]");
+    	        put("dataVersion", "0.1");
+    	    }
+    	}));
+    	customEventClient.sendEvents(events);
+        return "Custom event published successfully";
     }
     
 }
