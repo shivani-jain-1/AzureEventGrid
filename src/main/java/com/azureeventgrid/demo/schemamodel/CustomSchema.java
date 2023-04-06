d91db20e77f372286dac467f06400d23ad128f7f
0038X2744 <0038X2744@host.docker.internal>
2023-04-06 12:24:09 +0530

    custom schema

---
+++ src/main/java/com/azureeventgrid/demo/schemamodel/CustomSchema.java
@@ -0,0 +1,64 @@
+package com.azureeventgrid.demo.schemamodel;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CustomSchema {
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEventTime() {
+		return eventTime;
+	}
+	public void setEventTime(String eventTime) {
+		this.eventTime = eventTime;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getDataVersion() {
+		return dataVersion;
+	}
+	public void setDataVersion(String dataVersion) {
+		this.dataVersion = dataVersion;
+	}
+	public CustomSchema(int id, String eventTime, String subject, String eventType, String data, String dataVersion) {
+		super();
+		this.id = id;
+		this.eventTime = eventTime;
+		this.subject = subject;
+		this.eventType = eventType;
+		this.data = data;
+		this.dataVersion = dataVersion;
+	}
+	private int id; 
+	private String eventTime;
+	private String subject;
+	private String eventType;
+	private String data;
+	private String dataVersion;
+	
+	
+   }
+	
+
+
